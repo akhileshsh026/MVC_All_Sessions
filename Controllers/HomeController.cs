fdc5d838abd0d9207e02e38cb91c8cbccebe201f
akhileshsh026 <akhileshsh026@hotmail.com>
2020-02-27 18:52:42 +0530

    Employee CRUD operation Demo in this branch so project basics is inttilized

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,36 +16,6 @@ namespace MVC_All_Sessions.Controllers
             return View();
         }
 
-        [HttpPost]
-        public ActionResult Index(LoginInfo loginInfo)
-        {
-            if(ModelState.IsValid)
-            {
-
-                SqlConnection con = new SqlConnection(@"Server=localhost;Database=Mcop;User Id=sa;Password=Akhilesh@123;");
-                var Query = "select count(*) from LoginInfo where UserName=@uName and Password=@passwd";
-                SqlCommand cmd = new SqlCommand(Query, con);
-                cmd.Parameters.Add(new SqlParameter("@uName", loginInfo.UserName));
-                cmd.Parameters.Add(new SqlParameter("@passwd", loginInfo.Password));
-                con.Open();
-                int noOfUsers = (int)cmd.ExecuteScalar();
-                con.Close();
-                if (noOfUsers > 0)
-                {
-                    return RedirectToAction("Home");
-                } 
-                else
-                {
-                    ModelState.AddModelError("IC", "You Entered the Wrong Credianials..");
-                }
-            } 
-            
-            return View();
-        }
-
-        public ActionResult Home()
-        {
-            return View();
-        }
+ 
     }
 }
---
+++ Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_All_Sessions.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Salary {get; set; }
+        public int Deptid { get; set; }
+    }
+}
