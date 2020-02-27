49bcf1a0204649b9b01dfbbdebe658f1e9dd1916
akhileshsh026 <akhileshsh026@hotmail.com>
2020-02-27 14:56:24 +0530

    created the Home Action method view to get the response of the Index post method

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace MVC_All_Sessions.Controllers
         public ActionResult Index(string UserName , string Password)
         {
             SqlConnection con = new SqlConnection(@"Server=localhost;Database=Mcop;User Id=sa;Password=Akhilesh@123;");
-            var Query = "select count(*) from LoginInfo where UserName=@uName and Password=@psswd" ;
+            var Query = "select count(*) from LoginInfo where UserName=@uName and Password=@passwd" ;
             SqlCommand cmd = new SqlCommand(Query,con);
             cmd.Parameters.Add(new SqlParameter("@uName",UserName));
             cmd.Parameters.Add(new SqlParameter("@passwd",Password));
